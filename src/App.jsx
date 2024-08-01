306dcb06b5e8706765a8e245486f414f504ac68b
Chirag <92880442+chiragaiml21@users.noreply.github.com>
2024-08-01 10:35:06 +0000

    done some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,11 @@
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Header from "./components/Header";
-import Login from "./pages/Login";
-import Cart from "./pages/Cart";
-import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
-
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<Header />}>
-      <Route index element={<Home />} />
-      <Route path="about" element={<About />} />
-      <Route path="login" element={<Login />} />
-      <Route path="cart" element={<Cart />} />
-    </Route>
-  )
-)
+import Header from "./customer/components/Header/Header"
+import HomePage from "./customer/pages/Home/HomePage"
 
 function App() {
   return (
     <>
-      <RouterProvider router={router}/>
+      <Header/>
+      <HomePage/>
     </>
   )
 }
---
+++ src/customer/pages/Home/HomePage.jsx
@@ -0,0 +1,13 @@
+import HomeCarousel from "../../components/HomeCarousel/HomeCarousel"
+import ProductCategoriesComponent from "../../components/ProductCategories/ProductCategories"
+
+function HomePage() {
+  return (
+    <>
+      <HomeCarousel/>
+      <ProductCategoriesComponent/>
+    </>
+  )
+}
+
+export default HomePage;
